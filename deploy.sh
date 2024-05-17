8ccfe34805bbf124648aa08a094754bd02cdb050
preethivinyl <74344787+preethivinyl@users.noreply.github.com>
2024-05-17 17:36:12 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-if [[ $GIT_BRANCH == "dev" ]];then
+if [[ $GIT_BRANCH == "origin/dev" ]];then
 
 docker login -u preethivinil -p dckr_pat_mIuEI8WzwCR07FOt21xEsdigQlU
 docker tag final-project preethivinil/dev:project01
 docker push preethivinil/dev:project01
 
-elif [[ $GIT_BRANCH == "dev" ]];then
+elif [[ $GIT_BRANCH == "origin/master" ]];then
 
 docker login -u preethivinil -p dckr_pat_mIuEI8WzwCR07FOt21xEsdigQlU
 docker tag final-project preethivinil/prod:project01
