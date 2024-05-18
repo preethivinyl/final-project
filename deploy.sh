d8ca02e564576db833d83dfc1134f64cecce7916
preethivinyl <74344787+preethivinyl@users.noreply.github.com>
2024-05-18 16:35:09 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 if [[ $GIT_BRANCH == "origin/dev" ]];then
-
-sudo docker login -u preethivinil -p Beautifullife@14
-sudo docker tag final-project preethivinil/dev:project01
-sudo docker push preethivinil/dev:project01
+sh'chmod +x build.sh'
+sh'./build.sh'
+docker login -u preethivinil -p Beautifullife@14
+docker tag final-project preethivinil/dev:project01
+docker push preethivinil/dev:project01
 
 elif [[ $GIT_BRANCH == "origin/master" ]];then
-
-sudo docker login -u preethivinil -p Beautifullife@14
-sudo docker tag final-project preethivinil/prod:project01
-sudo docker push preethivinil/prod:project01
+sh'chmod +x build.sh'
+sh'./build.sh'
+docker login -u preethivinil -p Beautifullife@14
+docker tag final-project preethivinil/prod:project01
+docker push preethivinil/prod:project01
 
 fi
